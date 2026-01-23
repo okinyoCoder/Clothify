55878b87699f81a35e4fdb9af95765bd0cab4848
okinyoCoder <okinyoanthony@gmail.com>
2026-01-23 22:55:21 +0300

    created product module

--- Backend/clothify/products/models.py
+++ Backend/clothify/products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField( primary_key=True,default=uuid.uuid4, editable=False,)
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30, blank=False)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.PROTECT)
+    product_image = models.ImageField(upload_to='/item_images')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["created_at"]
+        indexes = [
+            models.index(fields=["name"]),
+            models.index(fields=["price"]),
+        ]
+
+    def __str__(self):
+        return f"{self.name} {self.price} {self.stock}"
